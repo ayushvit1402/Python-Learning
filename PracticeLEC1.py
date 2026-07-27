c71bbaf7cc61c0542450b42ed9723f2946f9c350
Ayush Kumar Mishra <ayushvit1402@gmail.com>
2026-07-27 10:48:44 +0530

    Add files via upload

---
+++ PracticeLEC1.py
@@ -0,0 +1,33 @@
+# print("Ayush")   #Print Your Name
+# print("VIT CHENNAI", "BTECH CSE DATA SCIENCE")  #Print your college name and Branch
+# Name=input("Enter Your Name: ")
+# Age=input("Enter your Age: ")
+# City=input("Enter City Name: ")
+# Subject=input("Favourite Subject: ")
+
+# a=int(input("First No: "))
+# b=int(input("Second No: "))
+# print(a+b, a-b, a*b, a/b, a%b,)
+
+# l=int(input("Enter the Length: "))
+# b=int(input("Enter the Bredth: "))
+# print("Area:",l*b)
+
+# a=int(input("Enter the side:"))
+# print("Area:",a**2)
+
+# r=int(input("Radius of the Circle: "))
+# print("Area:",3.14*r**2)
+
+# a=int(input("num1: "))
+# b=int(input("num2: "))
+# c=int(input("num3: "))
+# print("Average: ",(a+b+c)/3)
+
+# f=int(input("Enter Temp in Fahrenheit: "))
+# temp= (f-32)*5/9
+# print("Temp in Celcius: ",temp)
+
+# a=25
+# b=float(a)
+# print(type(b))
---
+++ QuestionLec1.py
@@ -0,0 +1,22 @@
+#Write a Program to input 2 numbers & print their sum
+a=int(input("Num1:"))
+b=int(input("Num2:"))
+Sum=a+b
+print(Sum)
+
+#Write a Program to input side of a square and print its area
+a=int(input("Side of the square:"))
+area=a*a
+print(area)
+
+#WAP to input 2 flaoting point no.s & print their average
+a=float(input("A="))
+b=float(input("B="))
+print((a+b)/2)
+
+
+# WAP to input 2 int numbers, a and b
+# Print True if a is greater than or equal to b. If not print False.
+a=int(input("A:"))
+b=int(input("B:"))
+print(a>=b)
