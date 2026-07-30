ba0d239d1cc93c02b1f5ed265c2822f2446972cb
Ayush Kumar Mishra <ayushvit1402@gmail.com>
2026-07-30 23:41:17 +0530

    Add files via upload

---
+++ Practice_Lec3.py
@@ -0,0 +1,26 @@
+# #1
+# name = input("Enter your name: ")
+# print(len(name))
+
+# #2
+# name1 = "ayush" 
+# name1_upper = name1.upper()
+# print(name1_upper)
+# name1_lower = name1.lower()
+# print(name1_lower)
+
+# #3
+# a = "School"
+# print(a[0])
+# print(a[5])
+
+#4
+str = "adsha"
+str = str.find("a")
+print(str)
+
+#5
+f = input("First Name:")
+l = input("Last Name:")
+f_l = f +" "+ l
+print(f_l)
