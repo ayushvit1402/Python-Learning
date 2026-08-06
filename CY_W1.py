fe1e969b0a2f9ea67c3917fdd303937da1998ca2
Ayush Kumar Mishra <ayushvit1402@gmail.com>
2026-08-06 23:58:45 +0530

    Add files via upload

---
+++ CY_W1.py
@@ -0,0 +1,18 @@
+#1.
+n = int(input())
+
+sum = n+3*n
+
+print("Sum:",sum)
+print("Rachel's statement is:",sum%4 == 0)
+
+#2.
+A = int(input())
+B = float(input())
+C = complex(input())
+
+print("Integer:",A)
+print("Floating-point number:",B)
+print("Complex number:",C)
+
+#3.
