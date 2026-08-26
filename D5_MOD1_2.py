6ad5d55b9d8622d6808fe8992f0dc4ec9634517a
Ayush Kumar Mishra <ayushvit1402@gmail.com>
2026-08-27 00:10:46 +0530

    Refactor word count and frequency counter code

--- D5_MOD1_2.py
+++ D5_MOD1_2.py
@@ -1,27 +1,27 @@
-# '''
-# 1.Word Count & Longest Word Finder
-# '''
-# n = input("Enter Sentence:").split()
-# print(f"Total Words: {len(n)}")
+'''
+1.Word Count & Longest Word Finder
+'''
+n = input("Enter Sentence:").split()
+print(f"Total Words: {len(n)}")
 
-# l = []
-# for a in n:
-#     s = len(a)
-#     l.append(s)
+l = []
+for a in n:
+    s = len(a)
+    l.append(s)
 
-# l.sort() 
-# b = l[-1]
+l.sort() 
+b = l[-1]
 
-# for i in n:
-#     if len(i) == b:
-#         print(f"Longest Word: {i} with Length:{len(i)}")
+for i in n:
+    if len(i) == b:
+        print(f"Longest Word: {i} with Length:{len(i)}")
 
-# '''or'''
+'''or'''
 
-# w = input("Enter Sentence:").split()
-# long_w = max(w, key=len)
-# print(f"Total Words: {len(w)}")
-# print(f"Longest Word:{long_w} with Length: {len(long_w)}")
+w = input("Enter Sentence:").split()
+long_w = max(w, key=len)
+print(f"Total Words: {len(w)}")
+print(f"Longest Word:{long_w} with Length: {len(long_w)}")
 
 '''
 2. Number Frequency Counter
@@ -29,14 +29,11 @@
 n = list(map(int, input("Enter NO.:").split()))
 t = int(input("Enter target no:"))
 
-l = []
-ind = []
-for i in n:
-    if i == t:
-        l.append(i)
-for j in n:
-        if j == t:
-             ind.append(l.index(j))
+a = []
+for i in range(len(n)):
+    if n[i] == t:
+        a.append(i)
 
-print(f"{t} appeared {len(l)} times.")
-print(f"Found at indices: {ind}")
+print(f"{t} appeared {len(a)} times.")
+print(f"Found at indices: {a}")
+
