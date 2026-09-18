a98b275368e9dc3264eb4c4c359c6663255e7632
Ayush Kumar Mishra <ayushvit1402@gmail.com>
2026-09-18 21:19:08 +0530

    Add files via upload

---
+++ 20_Functions.py
@@ -0,0 +1,39 @@
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a,b):
+    if(a>b):
+        print("First no. is Greater")
+    elif a==b:
+        print("EQUAL")
+    else:
+        print("Second no. is Greater")
+
+def isLesser(a,b):
+    pass
+
+a = 9
+b = 8
+calculateGmean(a,b)
+isGreater(a,b)
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
+
+c = 8
+d = 8
+calculateGmean(c,d)
+isGreater(c,d)
+# gmean2 = (c*d)/(c+d)
+# print(gmean2)
+
+'''
+-2 type of function
+1. Built in function
+ex- len(), min(), max()...etc
+2. User defined function
+ex-function using def 
+
+-Identation matters
+'''
+
